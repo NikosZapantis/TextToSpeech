16f401268aee89afb4b5234a70b243b4f5133701
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2023-04-28 14:43:36 +0300

    Voice selection {Added}

--- src/tts.js
+++ src/tts.js
@@ -1,7 +1,6 @@
 const inputField = document.getElementById("textInput");
 const submitButton = document.getElementById("submitBtn");
 
-
 submitButton.addEventListener("click", () => {
 
     if(inputField.value !== "") {
@@ -13,6 +12,14 @@ submitButton.addEventListener("click", () => {
 
 function textToSpeech(text) { //textToSpeech convertion
     let utterance = new SpeechSynthesisUtterance(text);
+    
+    // Set the selected voice
+    const selectedVoice = voiceSelection.options[voiceSelection.selectedIndex].value;
+    if (selectedVoice !== '') {
+        utterance.voice = voices[selectedVoice];
+        speechSynthesis.speak(utterance);
+    }
+
     speechSynthesis.speak(utterance);
 }
 
@@ -24,7 +31,8 @@ document.addEventListener("keydown", function(event) {
     
     if(validKeys.includes(keyValue)) {
         if(keyValue === "Enter" && !event.shiftKey) {
-            //?DONE todo: Find how to redirect to event listener;  
+            //?DONE todo: Find how to redirect to event listener; 
+            event.preventDefault(); 
             submitButton.click();
         }else if(keyValue === "Delete") {
             //reseting current input in the textarea
@@ -46,4 +54,56 @@ function resetCurrentInput() {
     document.getElementById("textInput").value = '';
 }
 
-//TODO: Selection between different voices
+//?DONE todo: Selection between different voices
+
+//Fetching the available voices from browser
+let voices = [];
+
+function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+    const voiceSelection = document.getElementById("voiceSelection");
+    
+    // Clear the current options in the selection box
+    while (voiceSelection.firstChild) {
+        voiceSelection.removeChild(voiceSelection.firstChild);
+    }
+
+    //Adding the default option
+    const defaultOption = document.createElement('option');
+    defaultOption.textContent = "--Select a voice--";
+    defaultOption.setAttribute('selected', 'selected');
+    defaultOption.setAttribute('disabled', 'disabled');
+    voiceSelection.appendChild(defaultOption);
+
+    //Populate the selection box with available voices
+    voices.forEach((voice, i) => {
+        const option = document.createElement('option');
+        option.textContent = voice.name + ' (' + voice.lang + ')';
+        option.setAttribute('value', i);
+        option.setAttribute('id', 'voiceSelectionBoxes'); // add ID attribute
+
+        voiceSelection.appendChild(option);
+    });
+}
+
+populateVoiceList();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+//Using the selected voice for TTS
+const voiceSelection = document.getElementById("voiceSelection");
+
+function textToSpeech(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    
+    //Set the selected voice
+    const selectedVoice = voiceSelection.options[voiceSelection.selectedIndex].value;
+    if (selectedVoice !== '') {
+    
+        utterance.voice = voices[selectedVoice];
+    }
+
+    speechSynthesis.speak(utterance);
+}
